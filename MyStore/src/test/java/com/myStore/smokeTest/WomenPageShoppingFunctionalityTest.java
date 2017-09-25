250bd992c5fb8f200c3e52d643e3c9e9a15a77e3
nayila <nayila1210@gmail.com>
2017-09-25 14:24:29 -0700

    This is my my store jave file

---
+++ MyStore/src/test/java/com/myStore/smokeTest/WomenPageShoppingFunctionalityTest.java
@@ -0,0 +1,182 @@
+package com.myStore.smokeTest;
+
+import org.testng.annotations.Test;
+
+import com.myStore.mainPage.AddressPage;
+import com.myStore.mainPage.MyStoreHomePage;
+import com.myStore.mainPage.OrderConfirmationPage;
+import com.myStore.mainPage.OrderHistorryPage;
+import com.myStore.mainPage.PaymentPage;
+import com.myStore.mainPage.PaymentSummaryPage;
+import com.myStore.mainPage.ShippingPage;
+import com.myStore.mainPage.SignInPage;
+import com.myStore.mainPage.SummaryPage;
+import com.myStore.womenPage.PrintedChiffonDressPage;
+import com.myStore.womenPage.WomenMainPage;
+import com.myStore.womenPage.YourAddressPage;
+import com.selenium.Nayilalibrary.BasePage;
+
+import jxl.common.Logger;
+
+public class WomenPageShoppingFunctionalityTest extends BasePage {
+
+	final static Logger logger = Logger.getLogger(WomenPageShoppingFunctionalityTest.class);
+	MyStoreHomePage myStoreHomePage = new MyStoreHomePage();
+	WomenMainPage myWomenMainPage = new WomenMainPage();
+	PrintedChiffonDressPage myPfDressPage = new PrintedChiffonDressPage();
+	SummaryPage mySummaryPage = new SummaryPage();
+	SignInPage mySigInPage = new SignInPage();
+	AddressPage myAddressPage = new AddressPage();
+	ShippingPage myShippingPage = new ShippingPage();
+	PaymentPage myPaymentPage = new PaymentPage();
+	PaymentSummaryPage myPaymentSummaryPage = new PaymentSummaryPage();
+	OrderConfirmationPage myOrderConfirmationPage = new OrderConfirmationPage();
+	YourAddressPage myYourAddressPage = new YourAddressPage();
+	OrderHistorryPage myOrderHistoryPage = new OrderHistorryPage();
+
+	@Test(enabled = false)
+	public void verifyShoppingFilterByPriceFunction() throws Exception {
+		logger.info("Test Case 3.1 Is Starting!");
+		myStoreHomePage.goto_MyStoreSite();
+		myStoreHomePage.click_OnWomenLink();
+		myWomenMainPage.wait_ForPageLoadComplete();
+		myWomenMainPage.select_OnFilter();
+		myWomenMainPage.scroll_DownWindow();
+		myWomenMainPage.compare_ItemPrice();
+		logger.info("Test Case 3.1 Is Ending!");
+
+	}
+
+	@Test(enabled = false)
+	public void verifyWomenPageShoppingFunction() throws Exception {
+		logger.info("Test Case 3.2 Is Starting!");
+		myStoreHomePage.goto_MyStoreSite();
+		myStoreHomePage.click_OnWomenLink();
+		myWomenMainPage.wait_ForPageLoadComplete();
+		myWomenMainPage.select_OnFilter();
+		myWomenMainPage.scroll_DownWindow();
+		myWomenMainPage.click_OnLowestItem();
+		myPfDressPage.wait_ForPageLoadComplete();
+		myPfDressPage.click_OnAddToChart();
+		myStoreHomePage.verify_AddTOCartSuccess("Printed Chiffon Dress");
+		myStoreHomePage.click_OnProceedToCheckOut();
+		mySummaryPage.wait_ForSummaryPageLoad();
+		mySummaryPage.click_OnCheckOutBtn();
+		mySigInPage.wait_ForPageLoadComplete();
+		mySigInPage.enter_UserNameAndPassW();
+		mySigInPage.click_OnSignIn();
+		myAddressPage.wait_ForPageLoadComplete();
+		myAddressPage.verify_personalInfo();
+		myAddressPage.click_OnPreceedToCheckOutBtn();
+		myShippingPage.wait_ForShippingPageLoad();
+		myShippingPage.click_OnTermOfService();
+		myShippingPage.click_OnPreceedToCheckOutBtn();
+		myPaymentPage.waitUntilPageLoadComplete();
+		myPaymentPage.click_OnBankWire();
+		myPaymentSummaryPage.wait_UntilPageLoadComplete();
+		myPaymentSummaryPage.click_OnConfirMyOrder();
+		myOrderConfirmationPage.wait_UntilPageLoadComplete();
+		logger.info("Test Case 3.2 Is Ending!");
+
+	}
+
+	@Test(enabled = false)
+	public void verifyUpdateAddressFunction() throws Exception {
+		logger.info("Test Case 3.3 Is Starting!");
+		myStoreHomePage.goto_MyStoreSite();
+		myStoreHomePage.click_OnWomenLink();
+		myWomenMainPage.wait_ForPageLoadComplete();
+		myWomenMainPage.select_OnFilter();
+		myWomenMainPage.scroll_DownWindow();
+		myWomenMainPage.click_OnLowestItem();
+		myPfDressPage.wait_ForPageLoadComplete();
+		myPfDressPage.click_OnAddToChart();
+		myStoreHomePage.verify_AddTOCartSuccess("Printed Chiffon Dress");
+		myStoreHomePage.click_OnProceedToCheckOut();
+		mySummaryPage.wait_ForSummaryPageLoad();
+		mySummaryPage.click_OnCheckOutBtn();
+		mySigInPage.wait_ForPageLoadComplete();
+		mySigInPage.enter_UserNameAndPassW();
+		mySigInPage.click_OnSignIn();
+		myAddressPage.wait_ForPageLoadComplete();
+		myAddressPage.verify_personalInfo();
+		myAddressPage.click_OnDeliveryAddressUpdate();
+		myYourAddressPage.wait_ForPageLoadComplete();
+		myYourAddressPage.edit_AddressOne();
+		myYourAddressPage.click_OnSaveBtn();
+		myAddressPage.wait_ForPageLoadComplete();
+		myAddressPage.verify_UpdatedpersonalInfo();
+		logger.info("Test Case 3.3 Is Ending!");
+
+	}
+
+	@Test(enabled = false)
+	public void verifyAddNewAddressFunction() throws Exception {
+		logger.info("Test Case 3.4 Is Starting!");
+		myStoreHomePage.goto_MyStoreSite();
+		myStoreHomePage.click_OnWomenLink();
+		myWomenMainPage.wait_ForPageLoadComplete();
+		myWomenMainPage.select_OnFilter();
+		myWomenMainPage.scroll_DownWindow();
+		myWomenMainPage.click_OnLowestItem();
+		myPfDressPage.wait_ForPageLoadComplete();
+		myPfDressPage.click_OnAddToChart();
+		myStoreHomePage.verify_AddTOCartSuccess("Printed Chiffon Dress");
+		myStoreHomePage.click_OnProceedToCheckOut();
+		mySummaryPage.wait_ForSummaryPageLoad();
+		mySummaryPage.click_OnCheckOutBtn();
+		mySigInPage.wait_ForPageLoadComplete();
+		mySigInPage.enter_UserNameAndPassW();
+		mySigInPage.click_OnSignIn();
+		myAddressPage.wait_ForPageLoadComplete();
+		myAddressPage.verify_UpdatedpersonalInfo();
+		myAddressPage.click_OnAddingNewAddress();
+		myYourAddressPage.wait_ForPageLoadComplete();
+		myYourAddressPage.add_NewAddress();
+		myYourAddressPage.click_OnSaveBtn();
+		myAddressPage.wait_ForPageLoadComplete();
+		myAddressPage.select_Allias();
+		myAddressPage.verify_NewPersonalInfo();
+		logger.info("Test Case 3.4 Is Ending!");
+
+	}
+
+	@Test(enabled = false)
+	public void verifyReorderFunction() throws Exception {
+		logger.info("Test Case 3.5 Is Starting!");
+		myStoreHomePage.goto_MyStoreSite();
+		myStoreHomePage.click_OnWomenLink();
+		myWomenMainPage.wait_ForPageLoadComplete();
+		myWomenMainPage.select_OnFilter();
+		myWomenMainPage.scroll_DownWindow();
+		myWomenMainPage.click_OnLowestItem();
+		myPfDressPage.wait_ForPageLoadComplete();
+		myPfDressPage.click_OnAddToChart();
+		myStoreHomePage.verify_AddTOCartSuccess("Printed Chiffon Dress");
+		myStoreHomePage.click_OnProceedToCheckOut();
+		mySummaryPage.wait_ForSummaryPageLoad();
+		mySummaryPage.click_OnCheckOutBtn();
+		mySigInPage.wait_ForPageLoadComplete();
+		mySigInPage.enter_UserNameAndPassW();
+		mySigInPage.click_OnSignIn();
+		myAddressPage.wait_ForPageLoadComplete();
+		myAddressPage.verify_personalInfo();
+		myAddressPage.click_OnPreceedToCheckOutBtn();
+		myShippingPage.wait_ForShippingPageLoad();
+		myShippingPage.click_OnTermOfService();
+		myShippingPage.click_OnPreceedToCheckOutBtn();
+		myPaymentPage.waitUntilPageLoadComplete();
+		myPaymentPage.click_OnBankWire();
+		myPaymentSummaryPage.wait_UntilPageLoadComplete();
+		myPaymentSummaryPage.click_OnConfirMyOrder();
+		myOrderConfirmationPage.wait_UntilPageLoadComplete();
+		myOrderConfirmationPage.click_OnBacktoOrder();
+		myOrderHistoryPage.wait_UntilPageLoadComplete();
+		myOrderHistoryPage.click_OnReorder();
+		mySummaryPage.wait_ForSummaryPageLoad();
+		mySummaryPage.click_OnDeleteBtn();
+		mySummaryPage.verify_OnDeleteBtn();
+		logger.info("Test Case 3.5 Is Ending!");
+
+	}
+}
---
+++ MyStore/src/test/java/com/myStore/womenPage/YourAddressPage.java
@@ -0,0 +1,101 @@
+package com.myStore.womenPage;
+
+import static org.testng.Assert.assertNotNull;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+
+import com.google.common.base.Function;
+import com.selenium.Nayilalibrary.BasePage;
+
+import jxl.common.Logger;
+
+public class YourAddressPage extends BasePage {
+	final static Logger logger = Logger.getLogger(YourAddressPage.class);
+
+	public YourAddressPage wait_ForPageLoadComplete() {
+		try {
+			logger.info("Step>Waiting For Page Load Complete");
+
+			Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(60, TimeUnit.SECONDS)
+					.pollingEvery(2, TimeUnit.SECONDS).ignoring(NoSuchElementException.class);
+			WebElement saveBtn = wait.until(new Function<WebDriver, WebElement>() {
+				public WebElement apply(WebDriver driver) {
+					return driver.findElement(By.id("submitAddress"));
+				}
+
+			});
+			assertNotNull(saveBtn);
+		} catch (Exception e) {
+			logger.error("Error!Waiting For Page Load is Failed");
+		}
+		return this;
+	}
+	
+	public YourAddressPage edit_AddressOne() {
+		logger.info("Step:>Edit Address One");
+		try {
+			Thread.sleep(1000);
+			JavascriptExecutor jse = (JavascriptExecutor) driver;
+			jse.executeScript("window.scrollBy(0,500)", "");
+			Thread.sleep(1000);
+			driver.findElement(By.id("address1")).clear();
+			driver.findElement(By.id("address1")).sendKeys("1212 Urumqi St");
+			driver.findElement(By.id("city")).clear();
+			driver.findElement(By.id("city")).sendKeys("Urumqi");
+		} catch (Exception e) {
+			logger.error("Error!! It Did Not Edit Address One");
+		}
+		return this;
+	}
+	public YourAddressPage click_OnSaveBtn() {
+		logger.info("Step:>Click On Save Btn");
+		try {
+			Thread.sleep(1000);
+			JavascriptExecutor jse = (JavascriptExecutor) driver;
+			jse.executeScript("window.scrollBy(0,250)", "");
+			Thread.sleep(1000);
+			driver.findElement(By.id("submitAddress")).click();
+		} catch (Exception e) {
+			logger.error("Error!! It Did Not Click On Save Btn");
+		}
+		return this;
+	}
+	
+	public YourAddressPage add_NewAddress() {
+		logger.info("Step:>Add New Address Info");
+		try {
+			Thread.sleep(1000);
+			JavascriptExecutor jse = (JavascriptExecutor) driver;
+			jse.executeScript("window.scrollBy(0,500)", "");
+			Thread.sleep(1000);
+			driver.findElement(By.id("address1")).clear();
+			driver.findElement(By.id("address1")).sendKeys("1212 Hulja St");
+			driver.findElement(By.id("city")).sendKeys("Hulja");
+			WebElement state=driver.findElement(By.id("id_state"));
+			Select sel=new Select(state);
+			sel.selectByValue("3");
+			driver.findElement(By.id("postcode")).sendKeys("54878");
+			driver.findElement(By.id("phone_mobile")).sendKeys("3124569847");
+			driver.findElement(By.id("alias")).clear();
+			driver.findElement(By.id("alias")).sendKeys("new address");
+			
+		} catch (Exception e) {
+			logger.error("Error!! It Did Not Add New Address Info");
+		}
+		return this;
+	}
+	
+	
+	
+	
+
+}
